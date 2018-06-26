a2f67644ff046275fa4fd831cd1377cce9efd558
Cristy Sillerico <cristysillerico@Cristys-MacBook-Pro.local>
2018-06-26 11:48:27 -0400

    First Push

---
+++ moment.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDKyCwkS_ZLeA6pe3NCYRO8ovDDg9mcIdI",
+    authDomain: "trainactivity-6bf9f.firebaseapp.com",
+    databaseURL: "https://trainactivity-6bf9f.firebaseio.com",
+    projectId: "trainactivity-6bf9f",
+    storageBucket: "trainactivity-6bf9f.appspot.com",
+    messagingSenderId: "885969366496"
+  };
+  firebase.initializeApp(config);
