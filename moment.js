8aab985be8cddece1310c2a7ab43117219b30b71
Cristy Sillerico <cristysillerico@Cristys-MacBook-Pro.local>
2018-06-30 22:00:39 -0400

    final

--- moment.js
+++ moment.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 var config = {
     apiKey: "AIzaSyDKyCwkS_ZLeA6pe3NCYRO8ovDDg9mcIdI",
     authDomain: "trainactivity-6bf9f.firebaseapp.com",
@@ -7,3 +8,81 @@ var config = {
     messagingSenderId: "885969366496"
   };
   firebase.initializeApp(config);
+// create a varibale to refernce the database
+ 
+  var database = firebase.database();
+
+  // capture button on click
+  $("#add-train-btn").on("click", function () {
+      event.preventDefault();
+
+      var trainName = $("#train-name-input").val().trim();
+      var destination = $("#destination-input").val().trim();
+      var firstTime = $("#start-input").val().trim();
+      var frequency = $("#frequency-input").val().trim();
+
+      var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+      console.log("FirstTimeConverted: " + firstTimeConverted);
+
+      var currentTime = moment();
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      var remainder = diffTime % frequency;
+      var minutesAway = frequency - remainder;
+      var nextTrain = moment().add(minutesAway, "minutes");
+      var nextArrival = moment(nextTrain).format("h:mm a");
+
+
+      // uploads new train data to the database
+      database.ref().push({
+          trainName: trainName,
+          destination: destination,
+          firstTime: firstTime,
+          remainder: remainder,
+          frequency: frequency,
+          minutesAway: minutesAway,
+          nextArrival: nextArrival
+      });
+
+      // clears all of the text boxes
+      $(".form-control").val("");
+
+      // logs everything to teh console
+      console.log("Current Time: " + moment(currentTime).format("HH:mm"));
+      console.log("Difference in Time: " + diffTime);
+      console.log("Remainder: " + remainder);
+      console.log("Minutes till Train: " + minutesAway);
+      console.log("nextTrain: " + nextTrain);
+      console.log("Arrival Time: " + moment(nextTrain).format("hh:mm"));
+
+  })
+  var trainCount = 0;
+  // firebase watcher + initial loader
+  // The createRow function takes data returned by firebase
+  database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+      console.log(childSnapshot.val());
+
+      // Get reference to exising tbody element, create new table row element
+      var tBody = $("tBody");
+      var tRow = $("<tr>");
+      var child = childSnapshot.val();
+
+      // methods run on jquery selectors return the selector they were run on
+      // this is why we can create and save a reference to a td in the same statement we update its text
+      var trainNameTd = $("<td>").text(child.trainName);
+      var destinationTd = $("<td>").text(child.destination);
+      var frequencyTd = $("<td>").text(child.frequency);
+      var nextArrivalTd = $("<td>").text(child.nextArrival);
+      var minutesAwayTd = $("<td>").text(child.minutesAway);
+
+
+
+
+
+      // append the newly created table data to the table row
+      tRow.append(trainNameTd, destinationTd, frequencyTd, nextArrivalTd, minutesAwayTd);
+      // append the table row to the table body
+      tBody.append(tRow);
+
+  })
+
+})
